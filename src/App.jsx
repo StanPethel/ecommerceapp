20864d8168dee9f77ba70a4e96b931b195211b93
Stanley Pethel <stanleypethel@yahoo.com>
2022-01-07 01:28:17 -0500

    added pay and success files to help with stripe pay

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Product from "./pages/Product";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Success from "./Success";
+import Pay from "./Pay";
 
 const App = () => {
-  return <Home/>
+  return (
+         <Router>
+          <Routes>
+            <Route path="/pay">
+              <Pay />
+              </Route>
+              <Route path="/success">
+                <Success />
+             </Route>
+           </Routes>
+          </Router>
+    );
           
 };
 
